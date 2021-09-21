c13b265ef5694941d225cd74aac8d6e4e283d2d3
Unique Rajak <uniquiebsccsit2074@Prime.edu.np>
2021-09-21 19:04:43 +0545

    Made encode and decode window

--- Steganography/Steganography.py
+++ Steganography/Steganography.py
@@ -1,10 +1,53 @@
-from math import radians
-import numpy as np     # installed with matplotlib
-import matplotlib.pyplot as plt
+from tkinter import *
 
-def main():
-    x = np.arange(0, radians(1800), radians(12))
-    plt.plot(x, np.cos(x), 'b')
-    plt.show()
 
-main()
+def encode():
+    main.destroy()
+    enc=Tk()
+    enc.title("Encode")
+    enc.geometry("500x400+300+150")
+
+    label1=Label(text="Secret message")
+    label1.place(relx=0.1,rely=0.1,height=20,width=100)
+
+    entry=Entry()
+    entry.place(relx=0.4,rely=0.1)
+
+    label2=Label(text="File")
+    label2.place(relx=0.1,rely=0.2,height=20,width=100)
+
+    entrySave=Entry()
+    entrySave.place(relx=0.4,rely=0.2)
+
+    buttonSelect=Button(text="Select File")
+    buttonSelect.place(relx=0.1,rely=0.3)
+
+    buttonEncode=Button(text="Encode")
+    buttonEncode.place(relx=0.5,rely=0.4)
+
+
+def decode():
+    main.destroy()
+    dec=Tk()
+    dec.title("Decode")
+    dec.geometry("500x400+300+150")
+
+    buttonSelect=Button(text="Select File")
+    buttonSelect.place(relx=0.1,rely=0.3)
+
+    buttonDecode=Button(text="Decode")
+    buttonDecode.place(relx=0.5,rely=0.4)
+
+
+main= Tk()
+main.title("Image Steganography")
+main.geometry("500x400+300+150")
+
+encodeButton=Button(text="Encode", command=encode)
+encodeButton.place(relx=0.3,rely=0.3,height=40, width=80)
+
+decodeButton=Button(text="Decode",command=decode)
+decodeButton.place(relx=0.5,rely=0.3,height=40, width=80)
+
+
+main.mainloop()
